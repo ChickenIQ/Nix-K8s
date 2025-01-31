43a701d6b3c7f6476779c4fc3157f1c8a124d8cb
ChickenIQ <emibenzar@gmail.com>
2025-01-31 22:31:05 +0200

    test

--- modules/disko.nix
+++ modules/disko.nix
@@ -1,47 +1,43 @@
 {
-  disko.devices = {
-    disk = {
-      main = {
-        type = "disk";
-        device = "/dev/disk/by-diskseq/1";
-        content = {
-          type = "gpt";
-          partitions = {
-            ESP = {
-              priority = 1;
-              name = "ESP";
-              start = "1M";
-              end = "1024M";
-              type = "EF00";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-                mountOptions = [ "umask=0077" ];
+  disko.devices.disk.main = {
+    type = "disk";
+    device = "/dev/vda";
+    content = {
+      type = "gpt";
+      partitions = {
+        boot = {
+          priority = 1;
+          name = "ESP";
+          start = "1M";
+          end = "1024M";
+          type = "EF00";
+          content = {
+            type = "filesystem";
+            format = "vfat";
+            mountpoint = "/boot";
+            mountOptions = [ "umask=0077" ];
+          };
+        };
+
+        system = {
+          size = "100%";
+          content = {
+            type = "btrfs";
+            extraArgs = ["-f"];
+            subvolumes = {
+              "root" = {
+                mountOptions = [ "compress=zstd" "noatime" ];
+                mountpoint = "/";
               };
-            };
 
-            root = {
-              size = "100%";
-              content = {
-                type = "btrfs";
-                extraArgs = [ "-f" ];
-                subvolumes = {
-                  "root" = {
-                    mountOptions = [ "compress=zstd" "noatime" ];
-                    mountpoint = "/";
-                  };
+              "nix" = {
+                mountOptions = [ "compress=zstd" "noatime" ];
+                mountpoint = "/nix";
+              };
 
-                  "nix" = {
-                    mountOptions = [ "compress=zstd" "noatime" ];
-                    mountpoint = "/nix";
-                  };
-
-                  "data" = {
-                    mountOptions = [ "compress=zstd" "noatime" ];
-                    mountpoint = "/data";
-                  };
-                };
+              "data" = {
+                mountOptions = [ "compress=zstd" "noatime" ];
+                mountpoint = "/data";
               };
             };
           };
@@ -49,4 +45,4 @@
       };
     };
   };
-}
+  };
