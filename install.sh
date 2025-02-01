93640a795e8336530dac5888475b784309288c88
ChickenIQ <emibenzar@gmail.com>
2025-02-02 00:26:48 +0200

    test

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 
-nixos-generate-config --show-hardware-config --no-filesystems --root /mnt
+nixos-generate-config --show-hardware-config --no-filesystems --root /mnt > ./modules/hardware-config.nix
 sudo nixos-install --no-root-password --flake ./#master
 
