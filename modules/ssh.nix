bed4a75e9a79c9e4e83c8f14c9b139f37412fd22
ChickenIQ <emibenzar@gmail.com>
2025-02-01 19:52:58 +0200

    test

--- modules/ssh.nix
+++ modules/ssh.nix
@@ -10,11 +10,11 @@
   users.users.root.openssh.authorizedKeys.keys = cfg.authorizedKeys;
 
   boot.loader = {
+    efi.canTouchEfiVariables = true;
     timeout = 0;
     grub = {
       enable = true;
       efiSupport = true;
-      efiInstallAsRemovable = true;
     };
   };
 }
