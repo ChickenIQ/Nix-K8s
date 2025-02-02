cffa8ee8bdc46731e90360fda4988a233ab2ee4e
ChickenIQ <emibenzar@gmail.com>
2025-02-02 03:46:11 +0200

    testf

--- modules/system.nix
+++ modules/system.nix
@@ -1,14 +1,20 @@
 { modulesPath, cfg, ... }:
 {
-  imports = [
-    (modulesPath + "/installer/scan/not-detected.nix")
-    (modulesPath + "/profiles/qemu-guest.nix")
-  ];
+  imports = [(modulesPath + "/installer/scan/not-detected.nix")];
 
   boot.loader = {
     timeout = 0;
     systemd-boot.enable = true;
     efi.canTouchEfiVariables = true;
+  };
+
+  nix = {
+    optimise.automatic = true;
+    settings = {
+      trusted-public-keys = ["nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="];
+      substituters = ["https://nix-community.cachix.org"];
+      experimental-features = "nix-command flakes";
+    };
   };
 
   system.stateVersion = cfg.stateVersion;
