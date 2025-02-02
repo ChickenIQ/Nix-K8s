b6b1e429a406068b827d53a3621f32720664d903
ChickenIQ <emibenzar@gmail.com>
2025-02-02 03:25:17 +0200

    test

--- modules/persist.nix
+++ modules/persist.nix
@@ -1,3 +1,4 @@
+{ config, ... }:
 {
   environment.persistence."/data" = {
     files = [ "/etc/machine-id" ];
@@ -7,4 +8,18 @@
       "/var/lib/nixos"
     ];
   };
+  boot.initrd.postResumeCommands = lib.mkAfter ''
+    delete_subvolume() {
+      IFS=$'\n'
+      for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
+        delete_subvolume "/temp/$i"
+      done
+      btrfs subvolume delete "$1"
+    }
+    mkdir /temp
+    mount ${config.disko.devices.disk.main.device} /temp
+    [[ -e /temp/root ]] && delete_subvolume "/temp/root"
+    btrfs subvolume create /temp/root
+    umount /temp
+  '';
 }
