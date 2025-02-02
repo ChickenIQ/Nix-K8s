f2285def41b55bd3cf127ab5fcadc72a3a67161a
ChickenIQ <emibenzar@gmail.com>
2025-02-02 03:52:45 +0200

    test

--- modules/system.nix
+++ modules/system.nix
@@ -1,6 +1,9 @@
 { modulesPath, cfg, ... }:
 {
-  imports = [(modulesPath + "/installer/scan/not-detected.nix")];
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+    (modulesPath + "/profiles/qemu-guest.nix")
+  ];
 
   boot.loader = {
     timeout = 0;
@@ -8,14 +11,14 @@
     efi.canTouchEfiVariables = true;
   };
 
-  nix = {
-    optimise.automatic = true;
-    settings = {
-      trusted-public-keys = ["nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="];
-      substituters = ["https://nix-community.cachix.org"];
-      experimental-features = "nix-command flakes";
-    };
-  };
+  # nix = {
+  #   optimise.automatic = true;
+  #   settings = {
+  #     trusted-public-keys = ["nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="];
+  #     substituters = ["https://nix-community.cachix.org"];
+  #     experimental-features = "nix-command flakes";
+  #   };
+  # };
 
   system.stateVersion = cfg.stateVersion;
 }
