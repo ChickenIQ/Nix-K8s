71bbd268ca42b542d98c131a928058ecbb59ca04
ChickenIQ <emibenzar@gmail.com>
2025-02-08 18:45:26 +0200

    test

--- modules/persistence.nix
+++ modules/persistence.nix
@@ -1,7 +1,11 @@
 {
   environment.persistence."/data" = {
-    files = ["/etc/machine-id"];
     hideMounts = true;
+    files = [
+      "/etc/ssh/ssh_host_ed25519_key"
+      "/etc/ssh/ssh_host_rsa_key"
+      "/etc/machine-id"
+    ];
     directories = [
       "/var/lib/systemd/coredump"
       "/var/lib/systemd/timers"
