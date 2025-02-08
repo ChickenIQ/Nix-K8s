ba2a68ad095377047e5e67d7e56a04164c0fedf2
ChickenIQ <emibenzar@gmail.com>
2025-02-08 19:15:12 +0200

    test

--- modules/persistence.nix
+++ modules/persistence.nix
@@ -2,7 +2,9 @@
   environment.persistence."/data" = {
     hideMounts = true;
     files = [
+      "/etc/ssh/ssh_host_ed25519_key.pub"
       "/etc/ssh/ssh_host_ed25519_key"
+      "/etc/ssh/ssh_host_rsa_key.pub"
       "/etc/ssh/ssh_host_rsa_key"
       "/etc/machine-id"
     ];
--- modules/system.nix
+++ modules/system.nix
@@ -1,4 +1,4 @@
-{ modulesPath, cfg, ... }:
+{ modulesPath, cfg, inputs, ... }:
 {
   boot.loader = {
     timeout = 0;
@@ -6,17 +6,24 @@
     efi.canTouchEfiVariables = true;
   };
 
-  nix.settings = {
-    trusted-public-keys = ["nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="];
-    substituters = ["https://nix-community.cachix.org"];
-    experimental-features = "nix-command flakes";
-    auto-optimise-store = true;
+  nix = {
+    optimise.automatic = true;
+    nixPath = ["nixpkgs=${inputs.nixpkgs}"];
+    settings = {
+      auto-optimise-store = true;
+      trusted-public-keys = ["nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="];
+      substituters = ["https://nix-community.cachix.org"];
+      experimental-features = "nix-command flakes";
+    };
   };
+  
+
+  environment.variables.FLAKE = cfg.flake;
 
   programs = {
     nix-index-database.comma.enable = true;
+    gamemode.enable = true;
     nh = {
-      flake = cfg.flake;
       enable = true;
       clean = {
         enable = true;
